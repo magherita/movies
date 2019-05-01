ea8006a455b80041165fbd17cf43e2e51d08e64b
Bugembe <andrewbugembe@neptunesoftwaregroup.com>
2019-05-01 17:19:36 +0300

    added login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -56,4 +56,41 @@ router.post("/register", (req, res) => {
         })
 });
 
+// @route POST api/users/login
+// @desc login users route
+// @access private
+router.post("/login", (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    // search user with email
+    User
+        .findOne({
+            email
+        })
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({
+                    email: "User not found"
+                });
+            }
+            // compare passwords
+            bcryptjs
+                .compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        res.json({
+                            msg: "Success"
+                        });
+                    } else {
+                        return res
+                            .status(400)
+                            .json({
+                                password: "Password incorrect"
+                            });
+                    }
+                })
+        })
+});
+
 module.exports = router;
